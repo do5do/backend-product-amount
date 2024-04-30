cc7933aa4b63c5ae7247a12235c0177deb7d4a64
do5do <do151015@gmail.com>
2024-04-30 12:43:47 +0900

    style: 사용하지 않는 import, 파일 제거

--- src/main/java/antigravity/repository/ProductRepository.java
+++ src/main/java/antigravity/repository/ProductRepository.java
@@ -1,10 +1,7 @@
 package antigravity.repository;
 
 import antigravity.domain.entity.Product;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
